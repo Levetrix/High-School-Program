38f899c57db82a87f734486aec7d1109c83b5fb0
Tyler Winzenried <winzenriedt@gmail.com>
2015-09-22 12:21:36 -0600

    Added faq to navigation

--- js/faq-custom.js
+++ js/faq-custom.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
-	console.log("ready");
 	$('a.clickScroll').on('click', function(event) {
-		console.log("a.scroll click even firing");
 		event.preventDefault();
 		var link = this;
-		console.log(link);
 		$.smoothScroll({
 			scrollTarget: link.hash
 		});
